c69cce0715ab93ec6f24b9b65234d2cd95528aea
IvMysl <IvMysl_2009@gmail.com>
2024-12-07 10:37:41 +0200

    done welcome

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import Hero from "@/components/Layout/Hero";
+import Hero from "@/components/Hero";
+import Welcome from "@/components/Welcome";
 
 export default function Home() {
   return (
     <>
     {/* <h1> container</h1> */}
     <Hero/>
+    <Welcome/>
     </>
   );
 }
