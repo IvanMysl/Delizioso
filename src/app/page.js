30cc609bbb11049ce58683868188ba98c6f853a0
IvMysl <IvMysl_2009@gmail.com>
2024-12-15 18:34:07 +0200

    done open

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Chef from "@/components/Chef";
 import Hero from "@/components/Hero";
 import Reserve from "@/components/Reserve";
 import Welcome from "@/components/Welcome";
+import Feedback from "@/components/Feedback";
+import Open from "@/components/Open";
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
     <Welcome/>
     <Reserve/>
     <Chef/>
+    <Feedback/>
+    <Open/>
     </>
   );
 }
